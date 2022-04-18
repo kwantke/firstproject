46e87cae611f369dde72ccf7db6f2e4a48e66e9b
kwantke <kokk7310@gamil.com>
2022-04-18 17:47:41 +0900

    customerjs 추가

--- TEST/react01/reactjstest01/src/App.js
+++ TEST/react01/reactjstest01/src/App.js
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customer = [
+    {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '고광규',
+    'birthday': '911122',
+    'gender': '남자',
+    'job': '대학생'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '홀길동',
+        'birthday': '911122',
+        'gender': '남자',
+        'job': '사무원'
+    },
+    {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '유재석',
+        'birthday': '911122',
+        'gender': '개발자',
+        'job': '대학생'
+    }
+
+]
 
 class App extends Component{
     render() {
         return (
-            <div className="gray-background">
-                <img src={logo} lat="logo" />
-                <h2>Let's develop management system!</h2>
+            <div>
+                {
+                    customer.map( c=> {
+                        return (
+                            <Customer
+                                key={c.id}
+                                id={c.id}
+                                image={c.image}
+                                name={c.name}
+                                birthday={c.birthday}
+                                gender={c.gender}
+                                job={c.job}
+                            />
+                        )
+                    })
+                }
             </div>
         );
     }
