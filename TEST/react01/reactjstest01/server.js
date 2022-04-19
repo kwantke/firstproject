3d60a50c205b83f9e8ce3cff1da4fb73eb8561a4
kwantke <kokk7310@gamil.com>
2022-04-20 05:21:40 +0900

    Add Get API Customers REST API Module

--- TEST/react01/reactjstest01/server.js
+++ TEST/react01/reactjstest01/server.js
@@ -6,8 +6,40 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello',  (req, res) =>{
+app.get('/api/customers', (req, res) =>{
+    res.send(
+        [
+            {
+                "id": 1,
+                "image": "https://placeimg.com/64/64/1",
+                "name": "고광규",
+                'birthday': "911122",
+                "gender": "남자",
+                "job": "대학생"
+            },
+            {
+                "id": 2,
+                "image": "https://placeimg.com/64/64/2",
+                "name": "타이거",
+                'birthday': "911122",
+                "gender": "여자",
+                "job": "변호사"
+            },
+            {
+                "id": 1,
+                "image": "https://placeimg.com/64/64/3",
+                "name": "임꺽정",
+                'birthday': "911122",
+                "gender": "남자",
+                "job": "대학생"
+            }
+
+        ]
+    )
+})
+
+/*app.get('/api/hello',  (req, res) =>{
     res.send({message: "Hello Express!"});
-});
+});*/
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
